92877231c1d752af775548ea9ad668c9eaad0a90
sava9ecode <sava9emode@yandex.ru>
2023-09-25 21:23:52 +0300

    Add postresql db and dockerize blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -71,13 +71,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "mysite.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
 
